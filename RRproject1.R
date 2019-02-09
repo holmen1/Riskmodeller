e631e1d6c3da018a05827cf908d1c68302c9b18e
holmen1 <holmen1@gmail.com>
2019-02-09 10:51:26 +0100

    fitdistr

--- RRproject1.R
+++ RRproject1.R
@@ -1,7 +1,10 @@
 ## Risk och reserv Projekt 1
 
+library(MASS)
+
 claims <- read.table("Projekt1_Grupp8.txt", header = TRUE, sep = ";")
 summary(claims)
+head(claims, n=10)
 
 ## ARRIVALS
 
@@ -22,19 +25,21 @@ hist(h2, breaks=b2)
 claims$Summer <- 0
 claims$Summer[b1[5]<claims$ClaimDay365&claims$ClaimDay365<=b1[9]] <- 1
 
-#Winter
-obs1w <- length(claims$ClaimCost[claims$ClaimType==1&claims$Summer==0])
-obs2w <- length(claims$ClaimCost[claims$ClaimType==2&claims$Summer==0])
-#Summer
-obs1s <- length(claims$ClaimCost[claims$ClaimType==1&claims$Summer==1])
-obs2s <- length(claims$ClaimCost[claims$ClaimType==2&claims$Summer==1])
+claims.daily <- aggregate(list(n=claims$ClaimDay),list(ClaimDay=claims$ClaimDay, ClaimDay365=claims$ClaimDay365, ClaimType=claims$ClaimType,Summer=claims$Summer), length)
+head(claims.daily, n=20)
 
 # Branch 1
-lambda1w <- obs1w / max(claims$ClaimDay[claims$ClaimType==1&claims$Summer==0])
-lambda1s <- obs1s / max(claims$ClaimDay[claims$ClaimType==1&claims$Summer==1])
+lambda1s<-fitdistr(claims.daily$n[claims.daily$ClaimType==1&claims.daily$Summer==1],"Poisson")$estimate
+lambda1w<-fitdistr(claims.daily$n[claims.daily$ClaimType==1&claims.daily$Summer==0],"Poisson")$estimate
 # Branch 2
-lambda2w <- obs2w / max(claims$ClaimDay[claims$ClaimType==2&claims$Summer==0])
-lambda2s <- obs2s / max(claims$ClaimDay[claims$ClaimType==2&claims$Summer==1])
+lambda2s<-fitdistr(claims.daily$n[claims.daily$ClaimType==2&claims.daily$Summer==1],"Poisson")$estimate
+lambda2w<-fitdistr(claims.daily$n[claims.daily$ClaimType==2&claims.daily$Summer==0],"Poisson")$estimate
+
+par(mfrow=c(2,2))
+plot(rpois(100,lambda1s))
+plot(rpois(100,lambda1w))
+plot(rpois(100,lambda2s))
+plot(rpois(100,lambda2w))
 
 
 days<-365
@@ -47,13 +52,6 @@ for(d in (1:days))
 }
 
 
-plot(rpois(365,lambda1w),type="p")
-plot(cumsum(rpois(365,lambda1w)))
-
-
-
-
-
 # COST
 claimsCount1Y <- aggregate(list(n=claims$ClaimDay365),list(ClaimType=claims$ClaimType,ClaimDay365=claims$ClaimDay365), length)
 check1 <- sum(claimsCount1Y$n[claimsCount1Y$ClaimType==1])
@@ -64,11 +62,25 @@ claimsCountY2 <- subset(claimsCount1Y, ClaimType == 2, select=c(ClaimDay365, n))
 par(mfrow=c(2,2))
 plot(claimsCountY1)
 plot(claimsCountY2)
-plot(rpois(365,lambda1))
+plot(rpois(365,lambda1s))
 plot(rpois(365,lambda2))
 
 
 ## COST
+par(mfrow=c(2,1))
+plot(claims$ClaimCost[claims$ClaimType==1])
+plot(claims$ClaimCost[claims$ClaimType==2])
+
+hist(claims$ClaimCost[claims$ClaimType==1],1000)
+hist(claims$ClaimCost[claims$ClaimType==2],1000)
+
+claims2<-claims$ClaimCost[claims$ClaimType==2]
+
+cost2<-subset(claims, ClaimType == 2, select=c(ClaimCost))
+claims[claims$ClaimType==2][order(claims$ClaimCost)]
+
+plot(c(1:length(cost2$ClaimCost)),sort(cost2$ClaimCost))
+
 claimsMeanCost10Y <- aggregate(list(MeanCost=claims$ClaimCost),list(ClaimType=claims$ClaimType,ClaimDay=claims$ClaimDay), mean)
 claimsMeanCost1Y <- aggregate(list(MeanCost=claims$ClaimCost),list(ClaimType=claims$ClaimType,ClaimDay365=claims$ClaimDay365), mean)
 
@@ -86,3 +98,8 @@ claimsCostY2 <- subset(claimsMeanCost1Y, ClaimType == 2, select=c(ClaimDay365, M
 par(mfrow=c(2,1))
 plot(claimsCostY1)
 plot(claimsCostY2)
+
+
+x<-rpois(100, lambda=10)
+x
+
