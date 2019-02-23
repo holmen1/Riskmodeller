c9d8d08609e112ce1361ceeb252296af92423b67
mhoc <mats.holm@amf.se>
2019-02-23 17:41:22 +0100

    ChainLadder1

--- RRproject2.R
+++ RRproject2.R
@@ -1,6 +1,7 @@
 ## Risk och reserv Projekt 2
 
-library(MASS)
+#library(MASS)
+library(ChainLadder)
 
 claims <- read.table("Projekt2_Grupp8.txt", header = TRUE, sep = ";")
 summary(claims)
@@ -29,25 +30,49 @@ claims.2 <- subset(claims,
                    select=c(ClaimDay,ClaimDay365,ClaimYear,PaymentDay,PaymentDay365,PaymentYear,ClaimCost))
 
 claims.1.yearly <- aggregate(ClaimCost ~ ClaimYear + PaymentYear, data=claims.1, FUN=sum)
-claims.2.yearly <- aggregate(ClaimCost ~ ClaimYear + PaymentYear, data=claims.1, FUN=sum)
+claims.2.yearly <- aggregate(ClaimCost ~ ClaimYear + PaymentYear, data=claims.2, FUN=sum)
 
-write.csv(claims.1.yearly, file = "claims1Y.csv")
-write.csv(claims.2.yearly, file = "claims2Y.csv")
+#write.csv(claims.1.yearly, file = "claims1Y.csv")
+#write.csv(claims.2.yearly, file = "claims2Y.csv")
+
+claims.1.yearly$Development <- claims.1.yearly$PaymentYear - claims.1.yearly$ClaimYear
+claims.2.yearly$Development <- claims.2.yearly$PaymentYear - claims.2.yearly$ClaimYear
+
+# Claim triangles 1 & 2
+m1 <- max(claims.1.yearly$ClaimYear) + 1
+n1 <- max(claims.1.yearly$Development) + 1
+T.1 <- matrix(0,m1,n1)
+for (m in 1:m1)
+{
+  for (n in 1:n1)
+  {
+    c <- claims.1.yearly$ClaimCost[claims.1.yearly$ClaimYear == (m-1) &
+                                   claims.1.yearly$Development == (n-1)]
+    T.1[m,n] <- max(0,c)
+  }
+}
+triangle.1 <- incr2cum(as.triangle(T.1))
+
+m2 <- max(claims.2.yearly$ClaimYear) + 1
+n2 <- max(claims.2.yearly$Development) + 1
+T.2 <- matrix(0,m2,n2)
+for (m in 1:m2)
+{
+  for (n in 1:n2)
+  {
+    c <- claims.2.yearly$ClaimCost[claims.2.yearly$ClaimYear == (m-1) &
+                                   claims.2.yearly$Development == (n-1)]
+    T.2[m,n] <- max(0,c)
+  }
+}
+triangle.2 <- incr2cum(as.triangle(T.2))
+
+
+plot(triangle.1)
+plot(triangle.2)
+#plot(triangle.1, lattice=TRUE)
 
 
 
 
-
-
-cost.daily <- aggregate(list(Cost=claims$ClaimCost),list(ClaimDay=claims$ClaimDay, ClaimType=claims$ClaimType), sum)
-
-
-
-
-claims.365<-subset(claims, select=c("ClaimType","ClaimDay"))
-
-
-
-arrivals.daily <- aggregate(list(Arrivals=claims$ClaimDay),list(ClaimDay=claims$ClaimDay, ClaimType=claims$ClaimType), length)
-cost.daily <- aggregate(list(Cost=claims$ClaimCost),list(ClaimDay=claims$ClaimDay, ClaimType=claims$ClaimType), sum)
 
