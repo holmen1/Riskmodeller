413f2f32290ac3954ae2d6a9ea7fbd5b21dcc3e2
mhoc <mats.holm@amf.se>
2019-03-05 19:55:50 +0100

    tisdag

--- RRproject2Mack2.R
+++ RRproject2Mack2.R
@@ -8,13 +8,16 @@ claims <- read.table("Projekt2_Grupp8.txt", header = TRUE, sep = ";")
 summary(claims)
 head(claims, n=10)
 
-sub.2 <- subset(claims, is.element(ClaimType, 2), select=c(ClaimDay, PaymentDay, ClaimCost))
+sub.2 <- subset(claims, is.element(ClaimType, 2),
+                select=c(ClaimDay, PaymentDay, ClaimCost))
 
 sub.2$ClaimYear <- sub.2$ClaimDay %/% 366 + 1
 sub.2$PaymentYear <- sub.2$PaymentDay %/% 366 + 1
 
-sub.2.yearly <- aggregate(ClaimCost ~ ClaimYear + PaymentYear, data=sub.2, FUN=sum)
-sub.2.yearly$Development <- sub.2.yearly$PaymentYear - sub.2.yearly$ClaimYear
+sub.2.yearly <- 
+  aggregate(ClaimCost ~ ClaimYear + PaymentYear, data=sub.2, FUN=sum)
+sub.2.yearly$Development <-
+  sub.2.yearly$PaymentYear - sub.2.yearly$ClaimYear
 
 # Last 10 years
 s.2.yearly <- subset(sub.2.yearly, ClaimYear > 10 & Development < 10)
@@ -31,28 +34,45 @@ f <- sapply(1:(n-1),
                }
 )
 
-sigma <- sapply(1:(n-1),
+sigma.sq <- sapply(1:(n-1),
             function(i){
               sum(T.2[c(1:(n-i)),i]*(T.2[c(1:(n-i)),i+1]/T.2[c(1:(n-i)),i] - f[i])^2)/(n-i-1)
             }
 )
 
+# Lazy way
 mack.2 <- MackChainLadder(T.2, est.sigma="Mack")
 mack.2
 mack.2$f
 mack.2$FullTriangle
 plot(mack.2)
 
-xtable(matrix(mack.2$FullTriangle,nrow=10,ncol=10))
 
-
-#4 First 10 years
-
+#First 10 years
 s.22.yearly <- subset(sub.2.yearly, ClaimYear < 11 & Development < 10)
 T.22 <- incr2cum(as.triangle(s.22.yearly,
                                    origin="ClaimYear",
                                    dev="Development",
                                    value="ClaimCost"), na.rm = FALSE)
+
+ultimate.claims <- rep(0, 9)
+U0 <- matrix(T.22,nrow=10,ncol=10)
+for (i in 2:10)
+{
+  tt <-2
+  U <- U0
+  for (r in i:10)
+  {
+    # Upper (left) triangle
+    U[r,(10+i-r):10] <- NA
+  }
+  m <- MackChainLadder(as.triangle(U), est.sigma="Mack")
+  ultimate.claims[i-1] <- m$FullTriangle[10,10]
+}
+
+t <- 1:9
+plot(t,ultimate.claims)
+lines(t,rep(U0[10,10],9))
 
 
 
